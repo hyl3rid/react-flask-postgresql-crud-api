1073e409fa6e3980969dfd8e100ec129514ba70a
hyl3rid <cliff.bam92@gmail.com>
2025-01-11 19:04:09 -0600

    update sh script

--- generate-db.sh
+++ generate-db.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 apt update
-apt install postgresql postgresql-contrib python3
+apt install -y postgresql postgresql-contrib python3
 systemctl start postgresql.service
+sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD 'newpassword';"
 sudo -u postgres createdb hyl3rid
-export DB_USERNAME="hyl3rid"
-export DB_PASSWORD="1234567890"
+export DB_USERNAME="postgres"
+export DB_PASSWORD="newpassword"
 python3 -m venv venv
 source venv/bin/activate
 python3 init_db.py
