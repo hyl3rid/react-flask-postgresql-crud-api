9934e2054e8c5493fe674ddf07669a79d4c2cee8
hyl3rid <cliff.bam92@gmail.com>
2025-01-11 20:45:43 -0600

    add python modules

--- generate-db.sh
+++ generate-db.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 apt update
 apt install -y postgresql postgresql-contrib python3
+python3 -m ensurepip --upgrade
+python3 -m pip install psycopg2-binary Flask Flask-Cors Flask-JWT-Extended
 systemctl start postgresql.service
 sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD 'newpassword';"
 sudo -u postgres createdb hyl3rid
