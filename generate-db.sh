4e8134dfc969a62fa9ac7dbdbc491cce8df014cf
hyl3rid <cliff.bam92@gmail.com>
2025-01-17 17:23:36 -0600

    remove comments

--- generate-db.sh
+++ generate-db.sh
@@ -1,9 +1,9 @@
 # !/bin/bash
-# apt update
-# apt install -y postgresql postgresql-contrib python3 python3-venv dbus-x11
-# python3 -m ensurepip --upgrade
-# python3 -m venv venv
-# python3 -m pip install psycopg2-binary Flask Flask-Cors Flask-JWT-Extended
+apt update
+apt install -y postgresql postgresql-contrib python3 python3-venv dbus-x11
+python3 -m ensurepip --upgrade
+python3 -m venv venv
+python3 -m pip install psycopg2-binary Flask Flask-Cors Flask-JWT-Extended
 source venv/bin/activate
 systemctl start postgresql.service
 sudo -u postgres psql -c "CREATE ROLE hyl3rid WITH SUPERUSER LOGIN CREATEDB CREATEROLE REPLICATION BYPASSRLS PASSWORD 'passw0rd';"
